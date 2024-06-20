322ea214e1c5294b3e6cab7b400c7c264ceabce5
RafayAhmad7548 <rafayahmad1425@gmail.com>
2024-06-20 19:14:32 +0500

    fixed gitignore

--- build/CMakeFiles/morebinds.dir/CMakeFiles/3.29.5/CompilerIdCXX/CMakeCXXCompilerId.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/morebinds.dir/CMakeFiles/3.29.5/CompilerIdCXX/CMakeCXXCompilerId.cpp.o: \
- /home/rafayahmad/Stuff/Coding/C++/morebinds/build/CMakeFiles/3.29.5/CompilerIdCXX/CMakeCXXCompilerId.cpp \
- /usr/include/stdc-predef.h
